06ec630dfd83a4d2d5dc5c60cf8c89615e6c503b
rbrick <rbrickproductions123@gmail.com>
2015-10-25 23:08:22 -0500

    add some usages

--- checksum.go
+++ checksum.go
@@ -1,46 +1,52 @@
 package main
 
 import (
-	"crypto/md5"
-	"crypto/sha1"
-	"crypto/sha256"
-	"crypto/sha512"
-	"flag"
-	"fmt"
-	"io/ioutil"
-	"os"
+    "crypto/md5"
+    "crypto/sha1"
+    "crypto/sha256"
+    "crypto/sha512"
+    "flag"
+    "fmt"
+    "io/ioutil"
+    "os"
 )
 
 func main() {
 
-	// What sums we need
-	checks := map[string]*bool{
-		"md5":    flag.Bool("md5", false, "flags"),
-		"sha1":   flag.Bool("sha1", false, "flags"),
-		"sha256": flag.Bool("sha256", false, "flags"),
-		"sha512": flag.Bool("sha512", false, "flags")}
+    // What sums we need
+    checks := map[string]*bool{
+        "md5":    flag.Bool("md5", false, "Get the md5 sum"),
+        "sha1":   flag.Bool("sha1", false, "Get the sha1 sum"),
+        "sha256": flag.Bool("sha256", false, "Get the sha256 sum"),
+        "sha512": flag.Bool("sha512", false, "Get the sha512 sum")}
 
-	funcs := map[string]func([]byte) interface{}{
-		"md5":    func(bytes []byte) interface{} { return md5.Sum(bytes) },
-		"sha1":   func(bytes []byte) interface{} { return sha1.Sum(bytes) },
-		"sha256": func(bytes []byte) interface{} { return sha256.Sum256(bytes) },
-		"sha512": func(bytes []byte) interface{} { return sha512.Sum512(bytes) },
-	}
+    funcs := map[string]func([]byte) interface{}{
+        "md5":    func(bytes []byte) interface{} { return md5.Sum(bytes) },
+        "sha1":   func(bytes []byte) interface{} { return sha1.Sum(bytes) },
+        "sha256": func(bytes []byte) interface{} { return sha256.Sum256(bytes) },
+        "sha512": func(bytes []byte) interface{} { return sha512.Sum512(bytes) },
+    }
 
-	// Parse the flag
-	flag.Parse()
+    // Parse the flag
+    flag.Parse()
 
-	// Parse the arguments
-	for _, i := range flag.Args() {
-		for k, v := range checks {
-			if *v {
-				if exists(i) {
-					data, _ := ioutil.ReadFile(i)
-					fmt.Printf("%s=%x [%s]\n", i, funcs[k](data), k)
-				}
-			}
-		}
-	}
+    if flag.NFlag() < 1 || flag.NArg() < 1 {
+        fmt.Println("checksum [-hashingAlgorithm...] [files...]")
+        flag.PrintDefaults()
+        return
+    }
+
+    // Parse the arguments
+    for _, i := range flag.Args() {
+        for k, v := range checks {
+            if *v {
+                if exists(i) {
+                    data, _ := ioutil.ReadFile(i)
+                    fmt.Printf("%s=%x [%s]\n", i, funcs[k](data), k)
+                }
+            }
+        }
+    }
 
 }
 
@@ -48,6 +54,6 @@ func main() {
   Tests if a file exists
 */
 func exists(s string) bool {
-	_, err := os.Stat(s)
-	return err == nil
+    _, err := os.Stat(s)
+    return err == nil
 }
